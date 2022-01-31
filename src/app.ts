a3e4ed2e59564a2b1e55c3f8f7bcf466aae09838
DevelopJKong <ybaram40@gmail.com>
2022-01-31 21:48:27 +0900

    고양이 데이터 모킹하기 완료

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.module";
 const app: express.Express = express();
-const port: number = 8000;
+const port: number = 5050;
 
-app.get("/", (req: express.Request, res:express.Response) => {
-  res.send("Hello world");
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ cats: Cat });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
