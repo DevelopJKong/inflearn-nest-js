f98e0160c9cb622682ce3765ac14961961839a41
DevelopJKong <ybaram40@gmail.com>
2022-02-03 20:26:52 +0900

    express 미들웨어 이해하기 완료

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
 import * as express from "express";
 import { Cat, CatType } from "./app.module";
 const app: express.Express = express();
-const port: number = 5050;
+const port: number = 8090;
+
+app.use((req,res,next) => {
+  console.log(req.rawHeaders[1]);
+  next();
+});
+
+app.get('/cats/som',(req,res,next) => {
+  console.log('this is som middleware');
+  next();
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
 });
+
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req,res,next) => {
+  console.log('this error middle');
+  res.send({error:'404 not found error'});
+  
+});
+
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
