a83efbfe98444584f8b15b15231fe119961cd969
DevelopJKong <ybaram40@gmail.com>
2022-03-03 09:40:29 +0900

    Repository 패턴과 레이어 분리 완료

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,10 @@
+import { ReadOnlyCatDto } from './dto/cat.dto';
 import { Body, UseFilters, UseInterceptors } from '@nestjs/common';
 import { Controller, Get, Post } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 import { CatsService } from './cats.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CatRequestDto } from './dto/cats.request.dto';
 
 @Controller('cats')
@@ -16,21 +18,34 @@ export class CatsController {
     return 'current cat';
   }
 
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error...',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    type: ReadOnlyCatDto,
+  })
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   async signUp(@Body() body: CatRequestDto) {
     return await this.catsService.signUp(body);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {
     return 'login';
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut() {
     return 'logout';
   }
 
+  @ApiOperation({ summary: '고양이 이미지 업로드' })
   @Post('upload/cats')
   uploadCatImg() {
     return 'uploadImg';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -7,9 +8,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const PORT = process.env.PORT;
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   const handleListening = () => {
-    console.log(`http://localhost${PORT}`);
+    console.log(`http://localhost:${PORT}`);
   };
   await app.listen(PORT, handleListening);
 }
