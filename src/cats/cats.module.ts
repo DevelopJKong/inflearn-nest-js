77c50c067a8a9954a711d26a3f88e6fcf54a5ece
DevelopJKong <ybaram40@gmail.com>
2022-03-07 09:43:49 +0900

    JWT와 로그인 서비스 & 순환 참조 모듈 [수정완료]

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,14 +1,22 @@
-import { CatsRepository } from './cats.repository';
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CatsController } from './cats.controller';
-import { Cat, CatSchema } from './cats.schema';
 import { CatsService } from './cats.service';
+import { forwardRef, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
+import { AuthModule } from 'src/auth/auth.module';
+import { CatsRepository } from './cats.repository';
+import { Cat, CatSchema } from './cats.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+  imports: [
+    MulterModule.register({
+      dest: './upload',
+    }),
+    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [CatsController],
   providers: [CatsService, CatsRepository],
-  exports: [CatsService],
+  exports: [CatsService, CatsRepository],
 })
 export class CatsModule {}
